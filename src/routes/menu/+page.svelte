7031e7e0df888152143d87ebf624e951577e5ee2
Sauhbagya Prasad <saubhagyaprasad31122001@gmail.com>
2023-05-02 03:17:26 +0530

    link

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -66,7 +66,7 @@
         on:mouseleave={hoverOut}
         rel="noreferrer"
         target="_blank"
-        href="https://react-next-blog-main.vercel.app/"
+        href="https://blog-rowinc.vercel.app/"
         class="menu-item">Blog</a
       >
       <a
